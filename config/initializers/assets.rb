344dbcc9b2a4083092208987a98de57778df221a
Philip Speake <philip_speake@yahoo.com>
2016-06-20 22:01:58 -0700

    heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,12 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
+%w(application ).each do |controller|
+
+
+  Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.scss"]
+end
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
