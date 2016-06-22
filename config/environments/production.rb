6a7e8abb37e225824078459e866d029b422cc1b4
Philip Speake <philip_speake@yahoo.com>
2016-06-21 21:48:16 -0700

    more work on css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   config.serve_static_assets = true
-  
+
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
