07343ffcbb40192cf4bc93bc5ceac56efb374f39
Philip Speake <philip_speake@yahoo.com>
2016-06-21 22:02:26 -0700

    added jquery for navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,23 +16,18 @@
 //= require bootstrap
 //= require_tree .
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
-    $('.navbar-toggle:visible').click();
+$(document).ready(function(){
+    var scroll_start = 0;
+    var startchange = $('.intro-heading');
+    var offset = startchange.offset();
+    if (startchange.length){
+        $(document).scroll(function() {
+            scroll_start = $(this).scrollTop();
+            if(scroll_start > offset.top) {
+                $(".navbar-default").css('background-color', 'black');
+            } else {
+                $('.navbar-default').css('background-color', 'transparent');
+            }
+        });
+    }
 });
